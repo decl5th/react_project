faacee7d94fa2a94adda01d601afcc49eb4ede52
decl5th <alsgur6624@gmail.com>
2024-04-16 17:52:28 +0900

    공통 컴포넌트 - 메인 제목, 버튼, 입력 박스 , 회원 가입 페이지 구성

---
+++ src/commons/components/Buttons.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { buttonColor } from "../../styles/color";
+import fontSize from "../../styles/fontSize";
+
+const {big} = fontSize;
+
+// 0 -> buttonColor의 첫번째 항목인 primary 컬러를 가져온다는 뜻
+export const BigButton = styled.button`
+    font-size: ${big};
+    height: 45px;
+    width: 100%;
+    border-radius: 3px;
+    cursor: pointer;
+  ${({color}) => 
+    buttonColor[color] && 
+    css`
+        background: ${buttonColor[color][0]};
+        color: ${buttonColor[color][1]};
+        border: 1px solid ${buttonColor[color][2]}
+        `}
+`;
+
+// 가입 버튼 배치 스타일링 설정 -> 다른 컴포넌트에서도 적용 가능한 버튼 배치 스타일링
+export const ButtonGroup = styled.div`
+    display: flex;
+    width: ${({width}) => width ? `${width}px` : '100%'};
+    margin: 20px auto;
+
+    button {
+        width: 0;
+        flex-grow: 1;
+
+    }
+
+    button + button {
+        margin-left: 7.5px;
+    }
+
+`;
